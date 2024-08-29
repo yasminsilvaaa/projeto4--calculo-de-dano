c455785007c1ebb70ba946bea1b307c99594deee
yasmin silva <yasmin.nascimento50@aluno.ce.gov.br>
2024-08-29 07:40:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let attckerName = prompt ('insira o nome do atacante');
+const attackPower = parselnt(prompt('insira o poder do ataque'))
+let defenderName = prompt ('insira o nome do defensor');
+let healthPoints =parselnt (prompt ('insira a quantidadede pontos de vida do defensor'));
+const hasShield = confirm('O defensor possui um escudo?');
+let damage;
+if (AttackPower>defensePower){
+  if(hasShield){
+    damage = attackPower - defensePower/2;
+    alert (${ attckerName atacou. ${defenderName} sofreu ${damage} de dano por possuir escudo.
+      Sua vida agora é $ {healthPoints - damage} . );
+            }else
+             damage = attackPower - defensePower 
+  alert ($ {attckerName} atacou.$ {defenderName} sofreu ${damage} de dano e não possuir escudo.Sua vida agora é $ {healthPoints - damage}.);
+}
+}else{
+  damage =0;
+  alert($ {attckerName} atacou, mas não afetou ${defenderName}. Nenhum dano causado!);
+  }
+healthPoints-= damage;
+  
