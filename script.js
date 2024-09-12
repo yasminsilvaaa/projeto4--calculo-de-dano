a75acbc1744dd39a02903e142ceda91bd1157578
yasmin silva <yasmin.nascimento50@aluno.ce.gov.br>
2024-09-12 08:50:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let attckerName = prompt('insira o nome do atacante');
-const attackPower = parselnt(prompt('insira o poder do ataque'))
-let defenderName = prompt('insira o nome do defensor');
-let healthPoints =parselnt(prompt ('insira a quantidadede pontos de vida do defensor'));
-const hasShield = confirm('O defensor possui um escudo?');
-let damage;
-if (attackPower>defensePower){
-  if(hasShield)
-    damage = attackPower - defensePower/2;
-    alert (${ attckerName atacou. ${defenderName} sofreu ${damage} de dano por possuir escudo.
-      Sua vida agora é $ {healthPoints - damage} .);
-             damage = attackPower - defensePower 
-  alert (${attckerName} atacou.$ {defenderName} sofreu ${damage} de dano e não possuir escudo.Sua vida agora é $ {healthPoints - damage}.);
-  damage =0;
-  alert(${attckerName} atacou, mas não afetou ${defenderName}. Nenhum dano causado!);
-  }
-healthPoints-= damage;
-  
+let char1 = 'john'
+let char2 = 'wick'
+let atk = 10
+let health = 10
+let def = 6
+let shield = false
+if (atk > def && shield == false ) {
+    atk = atk - def
+    health = health - atk
+    console.log(`${char1} atacou. ${char2} sofreu ${atk} de dano e não possui escudo. Sua vida agora é ${health}`)
+} else if (atk > def && shield == true) {
+    atk = atk - def
+    atk = atk / 2
+    health = health - atk
+    console.log(`${char1} atacou. ${char2} sofreu ${atk} de dano por possuir escudo. Sua vida agora é ${health}`)
+} else {
+    console.log(`${char1} atacou. mas não afetou ${char2}. Nenhum dano causado!`)
